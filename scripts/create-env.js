c980660a717205bf64948c90e20dd178c804ca14
AltiV <vuong.alan.t@gmail.com>
2020-12-31 01:26:07 -0500

    Attempting to bypass env obfuscation

--- scripts/create-env.js
+++ scripts/create-env.js
@@ -3,5 +3,5 @@ const fs = require('fs')
 console.log(process.env)
 fs.writeFileSync(
   './.env',
-  `REACT_APP_MUSIXMATCH_API_KEY = ${process.env.REACT_APP_MUSIXMATCH_API_KEY}\n`
+  `REACT_APP_MUSIXMATCH_API_KEY = ${process.env.REACT_APP_TEST}\n`
 )
