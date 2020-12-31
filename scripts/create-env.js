e880891f3bb4df49dc4b0af8c85030560b24ba21
AltiV <vuong.alan.t@gmail.com>
2020-12-31 13:07:29 -0500

    Update _redirects

--- scripts/create-env.js
+++ scripts/create-env.js
@@ -1,6 +1,8 @@
+// This file is used to create the .env file on Netlify's end
+
 const fs = require('fs')
 // Checking process.env variables on Netlify end
 fs.writeFileSync(
   './.env',
-  `REACT_APP_MUSIXMATCH_API_KEY = "${process.env.REACT_APP_TEST}"\n`
+  `REACT_APP_MUSIXMATCH_API_KEY = "${process.env.REACT_APP_MUSIXMATCH_API_KEY}"\n`
 )
