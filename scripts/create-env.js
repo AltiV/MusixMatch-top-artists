03bbc1bb615605cf4b67a0dc361ac3b5f793e1fc
AltiV <vuong.alan.t@gmail.com>
2020-12-31 01:19:16 -0500

    More process.env testing

--- scripts/create-env.js
+++ scripts/create-env.js
@@ -1,2 +1,7 @@
 const fs = require('fs')
-fs.writeFileSync('./.env', `REACT_APP_MUSIXMATCH_API_KEY=${process.env.REACT_APP_MUSIXMATCH_API_KEY}\n`)
+// Checking process.env variables on Netlify end
+console.log(process.env)
+fs.writeFileSync(
+  './.env',
+  `REACT_APP_MUSIXMATCH_API_KEY = ${process.env.REACT_APP_MUSIXMATCH_API_KEY}\n`
+)
